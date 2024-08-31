783d6daaab5a472e2853bea7b62cc088621ebd15
wanni30 <hubhy@nate.com>
2024-08-31 23:04:33 +0900

    캐릭터 움직이게 만들기(좌우로만)

--- lib/actors/player.dart
+++ lib/actors/player.dart
@@ -1,25 +1,67 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
+import 'package:flutter/services.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
 //  idle : 캐릭터가 움직이지 않고 가만히 있는 상태
 // running : 캐릭터가 움직이는 상태
 enum PlayerState { idle, running }
 
-class Player extends SpriteAnimationGroupComponent
-    with HasGameRef<PixelAdventure> {
-  String character;
-  Player({position, required this.character}) : super(position: position);
+// Player의 움직임의 방향을 설정
+enum PlayerDirection { left, right, none }
 
+class Player extends SpriteAnimationGroupComponent
+    with HasGameRef<PixelAdventure>, KeyboardHandler {
+  String character;
+  Player({
+    position,
+    this.character = 'Ninja Frog',
+  }) : super(position: position);
+
+  // idle, running
   late final SpriteAnimation idleAnimation;
   late final SpriteAnimation runningAnimation;
   final double stepTime = 0.05;
 
+  // PlayerDirection
+  PlayerDirection playerDirection = PlayerDirection.none;
+  double moveSpeed = 100;
+  Vector2 velocity = Vector2.zero();
+  bool isFacingRight = true;
+
+  // 화면에 나오게 하도록 초기값 셋팅  (initState())
   @override
   FutureOr<void> onLoad() {
     _onLoadAllAnimations();
     return super.onLoad();
+  }
+
+  // 어떻게  움직일지 초기값 셋팅 (initState())
+  @override
+  void update(double dt) {
+    _updatePlayerMovement(dt);
+    super.update(dt);
+  }
+
+  // 키보드로 이동을 어떻게 할지 초기값 셋팅 (initState())
+  @override
+  bool onKeyEvent(KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    final isLeftKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyA) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowLeft);
+    final isRightKeyPressed = keysPressed.contains(LogicalKeyboardKey.keyD) ||
+        keysPressed.contains(LogicalKeyboardKey.arrowRight);
+
+    if (isLeftKeyPressed && isRightKeyPressed) {
+      playerDirection = PlayerDirection.none;
+    } else if (isLeftKeyPressed) {
+      playerDirection = PlayerDirection.left;
+    } else if (isRightKeyPressed) {
+      playerDirection = PlayerDirection.right;
+    } else {
+      playerDirection = PlayerDirection.none;
+    }
+    return super.onKeyEvent(event, keysPressed);
   }
 
   void _onLoadAllAnimations() {
@@ -35,7 +77,7 @@ class Player extends SpriteAnimationGroupComponent
     };
 
     // Set current animation
-    current = PlayerState.idle;
+    current = PlayerState.running;
   }
 
   // 이렇게 메소드로 만들어두면 일일히 idle, running 상황에서 로직을 만들 필요없으니까
@@ -49,5 +91,34 @@ class Player extends SpriteAnimationGroupComponent
         textureSize: Vector2.all(32), // 이미지에 대한 사이즈를 조절하는거 같음
       ),
     );
+  }
+
+  void _updatePlayerMovement(double dt) {
+    double dirX = 0.0;
+    switch (playerDirection) {
+      case PlayerDirection.left:
+        if (isFacingRight) {
+          flipHorizontallyAroundCenter();
+          isFacingRight = false;
+        }
+        current = PlayerState.running;
+        dirX -= moveSpeed;
+        break;
+      case PlayerDirection.right:
+        if (!isFacingRight) {
+          flipHorizontallyAroundCenter();
+          isFacingRight = true;
+        }
+        current = PlayerState.running;
+        dirX += moveSpeed;
+        break;
+      case PlayerDirection.none:
+        current = PlayerState.idle;
+        break;
+      default:
+    }
+
+    velocity = Vector2(dirX, 0.0);
+    position += velocity * dt;
   }
 }
--- lib/pixel_adventure.dart
+++ lib/pixel_adventure.dart
@@ -2,23 +2,36 @@ import 'dart:async';
 import 'dart:ui';
 
 import 'package:flame/components.dart';
+import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import 'package:pixel_adventure/actors/player.dart';
 import 'package:pixel_adventure/levels/level.dart';
 
-class PixelAdventure extends FlameGame {
+// HasKeyboardHandlerComponents => 키보드에 대하여 제어하도록 컴포넌트 가져옴
+// DragCallbacks => 모바일 조이스틱에 대하여 제어하도록 컴포넌트 가져옴
+class PixelAdventure extends FlameGame
+    with HasKeyboardHandlerComponents, DragCallbacks {
   @override
   Color backgroundColor() => const Color(0xFF211F30);
 
   late final CameraComponent cam;
+  late JoystickComponent joystick;
+  // 모바일로 게임을 만들때는 true로 설정
+  // 웹, 컴퓨터로 만들때는 false로 설정
+  bool showJoystick = false;
 
-  final world = Level(
-    levelName: 'level-01.tmx',
-  );
+  Player player = Player(character: 'Mask Dude');
 
   @override
   FutureOr<void> onLoad() async {
     // Load all Images into cache
     await images.loadAllImages();
+
+    final world = Level(
+      player: player,
+      levelName: 'level-01.tmx',
+    );
 
     cam = CameraComponent.withFixedResolution(
         world: world, width: 640, height: 360);
@@ -26,6 +39,58 @@ class PixelAdventure extends FlameGame {
     cam.viewfinder.anchor = Anchor.topLeft;
 
     addAll([cam, world]);
+
+    if (showJoystick) {
+      addJoystick();
+    }
+
+    addJoystick();
+
     return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    if (showJoystick) {
+      updateJoystck();
+    }
+    super.update(dt);
+  }
+
+  void addJoystick() {
+    joystick = JoystickComponent(
+      knob: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/Knob.png'),
+        ),
+      ),
+      // knobRadius: 16, // 조이스틱 배경 밖으로 튀어가도록 하는 기능
+      background: SpriteComponent(
+        sprite: Sprite(
+          images.fromCache('HUD/JoyStick.png'),
+        ),
+      ),
+      margin: const EdgeInsets.only(left: 32, bottom: 32),
+    );
+
+    add(joystick);
+  }
+
+  void updateJoystck() {
+    switch (joystick.direction) {
+      case JoystickDirection.left:
+      case JoystickDirection.upLeft:
+      case JoystickDirection.downLeft:
+        player.playerDirection = PlayerDirection.left;
+        break;
+      case JoystickDirection.right:
+      case JoystickDirection.upRight:
+      case JoystickDirection.downRight:
+        player.playerDirection = PlayerDirection.right;
+        break;
+      default:
+        player.playerDirection = PlayerDirection.none;
+        break;
+    }
   }
 }
